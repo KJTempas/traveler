2989817b88e0ab89fc6b29fb45c2cee19771ca7a
KJTempas <fw6927mm@go.minneapolis.edu>
2020-02-04 14:26:23 -0600

    prints events in JList - but same for all cities

--- src/main/java/TicketGUI.java
+++ src/main/java/TicketGUI.java
@@ -10,9 +10,7 @@ public class TicketGUI extends JFrame {
     private JLabel topLabel;
     private JComboBox<String> cityChoices;
     private JLabel cityName;
-    private JLabel startDate;
     private JPanel mainPanel;
-    private JSpinner startDateSpinner;
 
     private JTable eventsTable;
     private DefaultTableModel tableModel;
@@ -23,7 +21,7 @@ public class TicketGUI extends JFrame {
     private DefaultListModel<String> listModel;
 
 
-//hashMap of cities and dmaID (which is used in query)
+    //hashMap of cities and dmaID (which is used in query)
     Map<String, String> cityAndDmaID = new HashMap<>();
 
     public TicketGUI() {
@@ -35,7 +33,7 @@ public class TicketGUI extends JFrame {
         DefaultComboBoxModel<String> cityModel = new DefaultComboBoxModel<>(cities);
         cityChoices.setModel(cityModel);
 
-        configureTableAtStart();
+        //configureTableAtStart();
         setHashMap();
         addListeners();
 
@@ -49,12 +47,12 @@ public class TicketGUI extends JFrame {
     }
 
     void setHashMap() {
-            cityAndDmaID.put("Boston", "235");
-            cityAndDmaID.put("Chicago", "249");
-            cityAndDmaID.put("Denver", "264");
-            cityAndDmaID.put("Kansas City", "311");
-            cityAndDmaID.put("Minneapolis/St.Paul", "336");
-        }
+        cityAndDmaID.put("Boston", "235");
+        cityAndDmaID.put("Chicago", "249");
+        cityAndDmaID.put("Denver", "264");
+        cityAndDmaID.put("Kansas City", "311");
+        cityAndDmaID.put("Minneapolis/St.Paul", "336");
+    }
 
 
     protected void addListeners() {
@@ -73,48 +71,57 @@ public class TicketGUI extends JFrame {
                 List<Embedded.Event> events = API.getEventsPerCity(dma);  //this is not getting to the API method
                 //send events list to method to convert it to vectors for display in the JTable
                 System.out.println("events back from API" + events);//vector of objects prints
-                Vector vectors = convertEventListToVector(events);
-                System.out.println("vectors after conversion from eventList" + vectors);  //prints vector
-                //updateEventsTable(vectors);
-                System.out.println(vectors.size()); //prints a number like 20
-                int length = vectors.size();
-                for(int i=0; i<vectors.size(); i++) {
-                    listModel.addElement(vectors[i]); //error -array type expected; found Vector
+                //Vector vectors = convertEventListToVector(events);
+                List<String> eventList = convertEventListToVector(events);
+                //System.out.println("vectors after conversion from eventList" + event);  //prints vector
+                System.out.println("list after conversion" + eventList);
+                for (String event : eventList) {
+                    listModel.addElement(event);
                 }
-                //for(int i=0; i<vectors.length, i++) { //length not recognized
-                //for(String event: vectors) {    //error-required object, found string
-                   // System.out.println(event);
-                    //listModel.addElement(event);
-               // }
-
+                //updateEventsTable(vectors);
+                //System.out.println(vectors.size()); //prints a number like 20
+                //int length = vectors.size();
+                // for(int i=0; i<vectors.size(); i++) {
+                //   listModel.addElement(vectors[i]); //error -array type expected; found Vector
             }
+            //for(int i=0; i<vectors.length, i++) { //length not recognized
+            //for(String event: vectors) {    //error-required object, found string
+            // System.out.println(event);
+            //listModel.addElement(event);
+            // }
+
 
         });
     }
 
 
-            private Vector convertEventListToVector(List<Embedded.Event> events) {
-                Vector data = new Vector();
-                Vector test = new Vector();
-                for (Embedded.Event e : events) {  //loop through events, and get name; add to the new vector v
-                    data.add(e.getName());
+    private List<String> convertEventListToVector(List<Embedded.Event> events) {
+        //Vector data = new Vector();
+        List<String> eventList = new ArrayList<>();
+        Vector test = new Vector();
+        for (Embedded.Event e : events) {  //loop through events, and get name; add to the new vector v
+            //data.add(e.getName());
+            eventList.add(e.getName());
 
-                }
+        }
 
-                System.out.println("Example vector"  + data);  //prints a vector
-                return data;
-            }
+        //System.out.println("Example vector"  + data);  //prints a vector
+        //return data;
+        System.out.println(eventList);
+        return eventList;
 
+    }
+}
 
-            Vector getColumnNames () {
+           /* Vector getColumnNames () {
             Vector colNames = new Vector();
             colNames.add("Event Name");
             colNames.add("Venue Name");
             columnNames = colNames;
 
             return columnNames;
-        }
-
+        }*/
+/*
         protected void configureTableAtStart (){
             eventsTable.setGridColor(Color.BLACK);
             eventsTable.setAutoCreateRowSorter(true);  //enables sorting
@@ -139,6 +146,6 @@ public class TicketGUI extends JFrame {
             System.out.println("vector  ?? " + tableModel.getDataVector());
         }
     }
-
+*/
 
 
